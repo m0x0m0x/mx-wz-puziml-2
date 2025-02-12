11ee61a10baea58e6b8e88698162b268c516d2bc
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-12 06:36:17 +0000

    🫦

---
+++ WO/hfz/h1/panty.py
@@ -0,0 +1,10 @@
+# Main Entry Point
+from src.uti import header1
+
+
+def main():
+    header1("Hello World")
+
+
+if __name__ == "__main__":
+    main()
---
+++ WO/hfz/h1/src/w1.py
@@ -0,0 +1,5 @@
+# Main work here
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
