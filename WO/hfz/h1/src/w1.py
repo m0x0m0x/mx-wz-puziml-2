b8121b3ef6aa42162768d9b9a7588ab309257b5b
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-12 11:56:00 +0000

    🫦

--- WO/hfz/h1/src/w1.py
+++ WO/hfz/h1/src/w1.py
@@ -4,3 +4,13 @@ from huggingface_hub import InferenceClient
 from dotenv import load_dotenv
 
 load_dotenv("src/.env")
+
+
+client.text_classification("Today is a great day")
+
+
+def inf_cl1():
+    client = InferenceClient(
+        "cardiffnlp/twitter-roberta-base-sentiment-latest",
+        token="hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    )
--- WO/hfz/uvinit.sh
+++ WO/hfz/uvinit.sh
@@ -37,7 +37,7 @@ uvsetup() {
     CO1="uv init $name_of_project"
     CO2="cd $name_of_project"
 
-    DEPS="rich requests python-dotenv"
+    DEPS="rich requests python-dotenv huggingface-hub"
     CO3="uv add  $DEPS"
     CO4="uv tree"
 
