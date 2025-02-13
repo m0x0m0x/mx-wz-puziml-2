3c0f5b2bc8d016c38de5be5905a5b99f7f998d77
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-13 11:12:07 +0000

    🫦

--- WO/gra/g1/src/w1.py
+++ WO/gra/g1/src/w1.py
@@ -12,6 +12,7 @@ def f1_greeter(name, intensity):
 
 def f1_greeter_launch(function=f1_greeter):
     f1l = gr.Interface(
+        title="Greeter",
         fn=function,
         inputs=["text", "slider"],
         outputs=["text"],
