b58280944268738abe015bae5503171e55501603
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-13 17:10:53 +0000

    🫦

--- WO/gra/g1/src/tu3.py
+++ WO/gra/g1/src/tu3.py
@@ -6,3 +6,16 @@ from .utilz import header1
 from rich.traceback import install
 
 install(show_locals=True)
+
+
+def gen_qr(data):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    return img
