c68294ae7db7ddb6b2a651a1b412a68768b378b2
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-13 10:41:54 +0000

    🫦

--- WO/gra/g1/src/w1.py
+++ WO/gra/g1/src/w1.py
@@ -1,2 +1,24 @@
 # Tutorial Function all in here
+import gradio as gr
+from rich.traceback import install
 
+install(show_locals=True)
+
+
+# -- Greeter Function
+def f1_greeter(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+
+def f1_greeter_launch():
+    iface = gr.Interface(
+        fn=f1_greeter,
+        inputs=[
+            gr.inputs.Textbox(name="name"),
+            gr.inputs.Slider(minimum=1, maximum=10, default=1, label="intensity"),
+        ],
+        outputs="text",
+        title="Greeter",
+        description="Say hello to someone!",
+    )
+    iface.launch()
