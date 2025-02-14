10e2584e3b1e9460694d18ca6c9d201df98f8cf1
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-14 19:16:39 +0000

    🫦

--- WO/gra/g5/test/test1.py
+++ WO/gra/g5/test/test1.py
@@ -8,16 +8,18 @@ with gr.Blocks() as demo:
     gr.Markdown("This is a test <br> Direct Hot reload <br> And Notty with he main")
 
     prompt = gr.Textbox(
-        label="Smell Panty Pussy SnifFStink",
+        label="Smell Panty Pussy ",
         type="text",
         placeholder="Smell Panty Pussy SnifFStink",
     )
     token = gr.Textbox(
-        "HF Token",
+        label="HF Token",
         type="password",
         placeholder="Enter your HF Token",
-        lines=1,
-        max_lines=1,
     )
+
+    with gr.Row():
+        generate_bth = gr.Button("Generate", variant="primary")
+
 
 demo.launch()
