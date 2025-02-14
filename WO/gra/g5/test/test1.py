801c03d510869121a239ac34f291ab1612705f8a
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-14 18:43:02 +0000

    🫦

--- WO/gra/g5/test/test1.py
+++ WO/gra/g5/test/test1.py
@@ -5,6 +5,10 @@ with gr.Blocks() as demo:
         "<div align='center'><img src='https://i.ibb.co/SX6n05rS/l.png' width='200'></div>"
     )
     gr.Markdown("<h1><center>Open Explorer</center></h1>")
-    gr.Markdown("This is a test <br> And herey <br> And Notty")
+    gr.Markdown("This is a test <br> Direct Hot reload <br> And Notty with he main")
+
+    prompt = gr.Textbox(
+        label="Smell Panty Pussy SnifFStink", placeholder="Smell Panty Pussy SnifFStink"
+    )
 
 demo.launch()
