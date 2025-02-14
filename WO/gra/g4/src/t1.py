beb76241a7a01fa254d0f883deae37dc89c9eea8
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-14 17:10:15 +0000

    🫦

--- WO/gra/g4/src/t1.py
+++ WO/gra/g4/src/t1.py
@@ -12,7 +12,55 @@ install(show_locals=True)
 
 
 def t1_main_runner():
-    hea2("t1.py - Gradio Test")
+    func_events()
 
 
 # ************************************
+
+# -- Example 1  - Greeter Function---
+
+
+def func_greet():
+    hea1("Hello Pussy Lickers")
+
+    def greet(name):
+        return f" {name}! Smell her Pussy "
+
+    with gr.Blocks() as demo:
+        gr.Markdown("# Welcome Panty Smeller")
+
+        with gr.Row():
+            inp = gr.Textbox(label="Entery Mistress name")
+            out = gr.Textbox(label="Panty Smell")
+
+        with gr.Column():
+            btn = gr.Button("Panty Smell")
+            gr.Markdown("Panty Smell add beloww")
+
+        btn.click(greet, inputs=inp, outputs=out)
+
+    demo.launch()
+
+
+# -- Example 2 - Blocks and Events  ---
+
+
+def func_events():
+    hea2("Events and Blocks")
+
+    def process_text(text):
+        return rprint(f"[green]Processed: {text.upper()}[/green]")
+
+    with gr.Blocks() as demo:
+        gr.Markdown("# Text Processing Demo")
+
+        with gr.Box():
+            inp = gr.Textbox(label="Enter Text", placeholder="Bastard Type Here")
+            out = gr.Textbox(label="Processed Text", value="", interactive=False)
+
+        btn = gr.Button("rape..")
+        btn.click(process_text, inputs=inp, outputs=out)
+
+        inp.change(process_text, inputs=inp, outputs=out)
+
+    demo.launch()
