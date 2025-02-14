09f8870a7b267ac01db65737ce57c3388d8a778c
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-14 18:36:19 +0000

    🫦

--- WO/gra/g5/test/test1.py
+++ WO/gra/g5/test/test1.py
@@ -1,8 +1,9 @@
 import gradio as gr
 
 with gr.Blocks() as demo:
-    gr.Markdown("![](https://i.ibb.co/SX6n05rS/l.png)")
-    gr.Markdown("<img src="https://i.ibb.co/SX6n05rS/l.png">")
+    gr.Markdown(
+        "<div align='center'><img src='https://i.ibb.co/SX6n05rS/l.png' width='200'></div>"
+    )
     gr.Markdown("<h1><center>Open Explorer</center></h1>")
 
 demo.launch()
