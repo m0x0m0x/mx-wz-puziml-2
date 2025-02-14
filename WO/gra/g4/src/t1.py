27e1a9134baf1d830f7aae3d0e7fc39716076a89
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-14 12:23:55 +0000

    🫦

--- WO/gra/g4/src/t1.py
+++ WO/gra/g4/src/t1.py
@@ -3,11 +3,22 @@
 import gradio as gr
 from .utilz import header1
 from rich.traceback import install
+from rich import print as rprint
 
 install(show_locals=True)
 
 
-# --- Main Function Runner ---
+# == Main Function Runner ===
 # This will run all the functions in this page which will be called in main
+
+
 def t1_main_runner():
     header1("T1 - Mastering Blocks in Gradio 2025 Ultimate Guide")
+    fn_1()
+
+
+# =======================================
+
+
+def fn_1():
+    rprint()
