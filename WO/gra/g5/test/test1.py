7cbde8bd59ab28342e3e279eb911217146faf2fb
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-14 19:13:17 +0000

    🫦

--- WO/gra/g5/test/test1.py
+++ WO/gra/g5/test/test1.py
@@ -8,7 +8,16 @@ with gr.Blocks() as demo:
     gr.Markdown("This is a test <br> Direct Hot reload <br> And Notty with he main")
 
     prompt = gr.Textbox(
-        label="Smell Panty Pussy SnifFStink", placeholder="Smell Panty Pussy SnifFStink"
+        label="Smell Panty Pussy SnifFStink",
+        type="text",
+        placeholder="Smell Panty Pussy SnifFStink",
+    )
+    token = gr.Textbox(
+        "HF Token",
+        type="password",
+        placeholder="Enter your HF Token",
+        lines=1,
+        max_lines=1,
     )
 
 demo.launch()
