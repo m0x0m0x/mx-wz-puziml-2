27188cc343ad5716baa6a925e43d83c4293bb054
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-14 11:33:25 +0000

    🫦

--- WO/gra/uvinit.sh
+++ WO/gra/uvinit.sh
@@ -37,7 +37,7 @@ uvsetup() {
     CO1="uv init $name_of_project"
     CO2="cd $name_of_project"
 
-    DEPS="rich python-dotenv gradio huggingface-hub marimo"
+    DEPS="rich python-dotenv gradio huggingface-hub"
     CO3="uv add  $DEPS"
     CO4="uv tree"
 
