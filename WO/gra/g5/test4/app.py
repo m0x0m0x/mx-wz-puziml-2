8e79c3c14ef6c61f1cd701e8cbb4d1b1871a291b
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-15 08:05:49 +0000

    🫦

--- WO/gra/g5/test4/app.py
+++ WO/gra/g5/test4/app.py
@@ -1,36 +1,8 @@
-import gradio as gr
-from src.utilz import hea1
-
-
-def create_interface():
-    hea1("This is a test")
-    with gr.Blocks() as demo:
-        gr.Markdown(
-            "<div align='center'><img src='https://i.ibb.co/SX6n05rS/l.png' width='200'></div>"
-        )
-        gr.Markdown("<h1><center>Open Explorer</center></h1>")
-        gr.Markdown("This is a test <br> Direct Hot reload <br> And Notty with he main")
-        gr.Markdown(" # Bladnista")
-
-        prompt = gr.Textbox(
-            label="Smell Janda ",
-            type="text",
-            placeholder="Smell Panty Pussy SnifFStink",
-        )
-        token = gr.Textbox(
-            label="HF Token",
-            type="password",
-            placeholder="Enter your HF Token",
-        )
-
-        with gr.Row():
-            generate_bth = gr.Button("Generate", variant="primary")
-
-    return demo
-
+from src.t1 import create_interface
 
 # Create the Gradio instance at module level
-demo = create_interface()
+run_create_interface = create_interface()
+demo = run_create_interface
 
 
 def main():
---
+++ WO/gra/g5/test4/src/t1.py
@@ -0,0 +1,31 @@
+# Adding main logic here and then called in app.py
+import gradio as gr
+from .utilz import hea1
+
+
+def create_interface():
+    hea1("This is a test")
+    with gr.Blocks() as demo:
+        gr.Markdown(
+            "<div align='center'><img src='https://i.ibb.co/SX6n05rS/l.png' width='200'></div>"
+        )
+        gr.Markdown("<h1><center>Open Explorer</center></h1>")
+        gr.Markdown("This is a test <br> Direct Hot reload <br> And Notty with he main")
+        gr.Markdown(" # Bladnista")
+        gr.Markdown(" > Booty Smeller")
+
+        prompt = gr.Textbox(
+            label="Smell Janda ",
+            type="text",
+            placeholder="Smell Panty Pussy SnifFStink",
+        )
+        token = gr.Textbox(
+            label="HF Token",
+            type="password",
+            placeholder="Enter your HF Token",
+        )
+
+        with gr.Row():
+            generate_bth = gr.Button("Generate", variant="primary")
+
+    return demo
