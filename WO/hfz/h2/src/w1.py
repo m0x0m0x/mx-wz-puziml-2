f6ede5eb4e97f4020a034e45878a7d4dd8208c3e
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-15 16:45:02 +0000

    🫦

--- WO/hfz/h2/src/w1.py
+++ WO/hfz/h2/src/w1.py
@@ -2,5 +2,13 @@
 
 from .ut import header1
 from rich.traceback import install
+from dotenv import load_dotenv
 
 install(show_locals=True)
+
+load_dotenv("src/.env")
+
+
+# File Main function
+def w1_main():
+    header1("Hello World")
