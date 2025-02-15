9562ab3a5b76abe3713dd2a0ed600491f4d1b5a0
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-15 07:45:40 +0000

    🫦

--- WO/gra/g5/test3/app.py
+++ WO/gra/g5/test3/app.py
@@ -4,14 +4,13 @@ from src.utilz import hea1
 
 def zunc_1():
     hea1("This is a test")
-    with gr.Blocks() as demo:
+    with gr.Blocks() as demo:  # This local variable needs to be exposed
         gr.Markdown(
             "<div align='center'><img src='https://i.ibb.co/SX6n05rS/l.png' width='200'></div>"
         )
         gr.Markdown("<h1><center>Open Explorer</center></h1>")
         gr.Markdown("This is a test <br> Direct Hot reload <br> And Notty with he main")
-        gr.Markdown("--> New Entery")
-        gr.Markdown("Zandango.launch()")
+        gr.Markdown(" # DOUBLE MAN")
 
         prompt = gr.Textbox(
             label="Smell Janda ",
@@ -30,6 +29,9 @@ def zunc_1():
     return demo
 
 
+# Create the Gradio demo instance at module level
+demo = zunc_1()
+
 # Main Execution
 if __name__ == "__main__":
-    zunc_1().launch()
+    demo.launch()
