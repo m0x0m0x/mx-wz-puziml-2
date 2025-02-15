b0ba77ee4fb5091860a7a7682b90b54fb94c4dd7
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-15 08:22:36 +0000

    🫦

--- WO/gra/g5/test4/src/t1.py
+++ WO/gra/g5/test4/src/t1.py
@@ -6,9 +6,7 @@ from .utilz import hea1
 def create_interface():
     hea1("This is a test")
     with gr.Blocks() as demo:
-        gr.Markdown(
-            "<div align='center'><img src='https://i.ibb.co/SX6n05rS/l.png' width='200'></div>"
-        )
+        gr.Markdown("<div align='center'><img src='l.png' width='200'></div>")
         gr.Markdown("<h1><center>Open Explorer</center></h1>")
         gr.Markdown("This is a test <br> Direct Hot reload <br> And Notty with he main")
         gr.Markdown(" # Bladnista")
